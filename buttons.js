00a0a985cc85d207c2a7d94233bddcc29666dfd0
Тим Муранов <139687295+TimmMuranov@users.noreply.github.com>
2024-09-29 21:13:57 +0300

    Update buttons.js
    Значительно опримизировал и добавил поле для размера. Добавил пока что нерабочие кнопки создания прямой, квадрата и круга.

--- buttons.js
+++ buttons.js
@@ -1,33 +1,37 @@
 const rabbish = document.getElementById("rabbish");
 const pen = document.getElementById("pen");
+var p = document.getElementById("p");//окно размера у ручки
+var r = document.getElementById("r");//окно размера у ластика
+p.value = 1;
+r.value = 20;
 
 pen.addEventListener('click', async () => {
   pen.style.backgroundColor = "red";
   rabbish.style.backgroundColor = "gray";
-  let penSize = parseInt(prompt('Введите ширину ручки (1-20)'));
-  if (!isNaN(penSize) && penSize > 0 && penSize <= 20) {
-    alert('Ручка включена. Ширина ' + penSize);
-    // Вызываем контекст из функции обратного вызова
-    context.lineWidth = penSize;
-    context.strokeStyle = "black";
-  } else {
-    context.lineWidth = 1;
-    context.strokeStyle = "black";
-    alert("Некорректный ввод. Ширина ручки по умолчанию 1");
-  }
+  context.lineWidth = p.value;
+  context.strokeStyle = "black";
 });
 
 rabbish.addEventListener('click', async () => {
   pen.style.backgroundColor = "gray";
   rabbish.style.backgroundColor = "red";
-  let rabbishSize = parseInt(prompt('Введите ширину ластика (1-100)'));
-  if (!isNaN(rabbishSize) && rabbishSize > 0 && rabbishSize <= 100) {
-    alert('Ластик включен. Ширина ' + rabbishSize);
-    context.lineWidth = rabbishSize;
-    context.strokeStyle = "white";
-  } else {
-    context.lineWidth = 10;
-    context.strokeStyle = "white";
-    alert("Некорректный ввод. Ширина ручки по умолчанию 10");
-  }
+  context.lineWidth = r.value;
+  context.strokeStyle = "white";
 });
+
+function updateInput(){
+  if(p.value < 0 || p.value > 20 || isNaN(p.value) || p.value === ''){
+      p.value = 1;
+      alert('Недопустимое значение ручки. Установлено значение по умолчанию (1).');
+    }
+  if(r.value < 0 || r.value > 100 || isNaN(r.value) || r.value === ''){
+    r.value = 20;
+    alert('Недопустимое значение ластика. Установлено значение по умолчанию (20).');
+  }
+  if(pen.style.backgroundColor === "red"){
+    context.lineWidth = p.value;
+  }
+  else if(rabbish.style.backgroundColor === "red"){
+    context.lineWidth = r.value;
+  }
+}
